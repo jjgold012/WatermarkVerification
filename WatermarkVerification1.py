9610aa2c061f6d37a9cf2f1366d60f7afa53dc69
JJG <j.j.goldberger@gmail.com>
2019-04-29 12:58:31 +0300

    move stuff to utils

--- WatermarkVerification1.py
+++ WatermarkVerification1.py
@@ -1,39 +1,26 @@
 # from nn_verification.utils import load_model
 import numpy as np
 import os
-import sys
-sys.path.insert(0,'..')
-import uuid
-from nn_verification.utils import load_model
 import argparse
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import backend as K
-from maraboupy import Marabou
 import utils
+from maraboupy import Marabou
+from maraboupy import MarabouUtils
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help='the name of the model')
-    parser.add_argument('--input_path', default='../nn_verification/data/wm.set.npy', help='input file path')
-
-    args = parser.parse_args()
-
+def run(args):
     model_name = args.model
     net_model, submodel, last_layer_model = utils.splitModel(model_name)
     
     wm_images = np.load(args.input_path)  
     input_test = np.reshape(wm_images[1], (1,28,28,1))
     
-    print(last_layer_model.predict(submodel.predict(input_test)))
-    print(net_model.predict(input_test))
+    # print(last_layer_model.predict(submodel.predict(input_test)))
+    # print(net_model.predict(input_test))
 
     filename = utils.saveModelAsProtobuf(last_layer_model, 'last.layer.{}'.format(model_name))
     network = Marabou.read_tf_weights_as_var(filename=filename, inputVals=submodel.predict(input_test))
     
     # network.setUpperBound(network.outputVars[0][0], 5)
-    epsilon = 0
+    epsilon = float(args.epsilon)
     for k in network.matMulLayers.keys():
         n, m = network.matMulLayers[k]['vals'].shape
         print(n,m)
@@ -64,3 +51,13 @@ if __name__ == '__main__':
     print(network.numVars)
 
     print(len(network.equList))
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='the name of the model')
+    parser.add_argument('--input_path', default='../nn_verification/data/wm.set.npy', help='input file path')
+    parser.add_argument('--epsilon', default=0.0, help='epsilon to use')
+    parser.add_argument('--epsilon_interval', default=0.01, help='epsilon smallest change')
+    args = parser.parse_args()
+    run(args)
--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ from tensorflow.python.framework import graph_io
 def splitModel(model_name):
     MODELS_PATH = '../Models'
     net_model = load_model(os.path.join(MODELS_PATH, model_name+'_model.json'), os.path.join(MODELS_PATH, model_name+'_model.h5'))
-    print('------------------Model Summary------------------\n')
+    print('\n------------------Model Summary------------------\n')
     print(net_model.summary())
 
     submodel = keras.Model(inputs=net_model.inputs, outputs=net_model.layers[-2].output, name='submodel-'+str(uuid.uuid4())[:5])
@@ -27,7 +27,7 @@ def splitModel(model_name):
     last_layer_model = keras.Model(inputs=last_layer_in, outputs=net_model.layers[-1](last_layer_in), name='lastlayer-'+str(uuid.uuid4())[:5])
     print('\n------------------Last Layer Summary------------------\n')
     print(last_layer_model.summary())
-    print('\n------------------------------------------------------')
+    print('\n------------------------------------------------------\n')
 
     return net_model, submodel, last_layer_model
 
@@ -36,8 +36,8 @@ def saveModelAsProtobuf(last_layer_model, model_name):
     file_writer = tf.summary.FileWriter('./logs', sess.graph)
     output_node_names = [node.name[:-2] for node in last_layer_model.outputs]
     constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), output_node_names)
-    graph_io.write_graph(constant_graph, '../ProtobufNetworks', '{}.pb'.format(model_name), as_text=False)
-    print('saved the constant graph (ready for inference) at: ', os.path.join('ProtobufNetworks', '{}.pb'.format(model_name)))
+    graph_io.write_graph(constant_graph, './ProtobufNetworks', '{}.pb'.format(model_name), as_text=False)
+    print('Saved the model at: ', os.path.join('ProtobufNetworks', '{}.pb'.format(model_name)))
 
-    filename = '../ProtobufNetworks/{}.pb'.format(model_name)
+    filename = './ProtobufNetworks/{}.pb'.format(model_name)
     return filename
