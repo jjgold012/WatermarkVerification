a5d8e0bd4d708a75708dc20a04794c5ad76ce56d
JJG <j.j.goldberger@gmail.com>
2019-05-13 12:39:37 +0300

    add test for apply new weights

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from copy import deepcopy
 from maraboupy import Marabou
 from maraboupy import MarabouUtils
 from WatermarkVerification1 import *
-
-
+from copy import deepcopy
+from tensorflow import keras
 
 model_name = 'mnist.w.wm'
 net_model, submodel, last_layer_model = utils.splitModel(model_name)
@@ -17,6 +17,7 @@ filename = utils.saveModelAsProtobuf(last_layer_model, 'last.layer.{}'.format(mo
 inputs = np.load('../nn_verification/data/wm.set.npy')
 epsilon_max = 0.5
 epsilon_interval = 0.2 
+out_file = open("test.out", "w")
 for i in range(5):
 
     input_test = np.reshape(inputs[i], (1,28,28,1))
@@ -25,6 +26,15 @@ for i in range(5):
     network = Marabou.read_tf_weights_as_var(filename=filename, inputVals=submodel.predict(input_test))
     
     unsat_epsilon, sat_epsilon, sat_vals = findEpsilonInterval(epsilon_max, epsilon_interval, network, prediction)
-    epsilonsVars = network.matMulLayers[0]['epsilons']
-    # epsilonsVals = set(map(lambda v: sat_vals[1][2][v], epsilonsVars))
-    epsilonsVals = np.array([[sat_vals[1][1][0][epsilonsVars[j][i]] for i in range(epsilonsVars.shape[1])] for j in range(epsilonsVars.shape[0])])
+    epsilons_vars = network.matMulLayers[0]['epsilons']
+    all_vals = sat_vals[1][max(sat_vals[1].keys())][0]
+    sat_out = [all_vals[i] for i in range(10)]
+    epsilons_vals = np.array([[all_vals[epsilons_vars[j][i]] for i in range(epsilons_vars.shape[1])] for j in range(epsilons_vars.shape[0])])
+    weights = last_layer_model.get_weights()[0]
+    new_weights = weights + epsilons_vals
+    c = keras.models.clone_model(last_layer_model)
+    c.set_weights([new_weights])
+    out_file.write('{}\n'.format(np.array(sat_out)))
+    out_file.write('{}\n'.format(c.predict(submodel.predict(input_test))[0]))
+out_file.close()
+
