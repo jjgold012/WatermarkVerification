042d4e75e1b11b429c83ed86714354e4ddb733c0
JJG <j.j.goldberger@gmail.com>
2019-06-04 11:40:51 +0300

    run file for cluster

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # A simple script to run the verification jobs
 
+export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
+
 exec python WatermarkVerification2.py --model mnist.w.wm --epsilon_max 100
