83b530cf45852353497e7ce9caa04ce61b87d34c
JJG <j.j.goldberger@gmail.com>
2019-06-19 18:41:11 +0300

    try with dummy problem

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 #SBATCH --killable
 #SBATCH -c2
 #SBATCH --time=2-0
+#SBATCH --mem=2g
 
 export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
 
-#python3 WatermarkVerification2.py --model mnist.w.wm --epsilon_max 100
-python3 WatermarkVerification2.py --model test --input_path ./test_data/test_images.npy --epsilon_max 100
+python3 WatermarkVerification2.py --model mnist.w.wm --epsilon_max 100
+# python3 WatermarkVerification2.py --model test --input_path ./test_data/test_images.npy --epsilon_max 100
