4643271bfba1d0bcecb39afe45c5cac85e0f909d
JJG <j.j.goldberger@gmail.com>
2019-06-23 10:59:18 +0300

    test setup

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@
 export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
 
 # python3 WatermarkVerification2.py --model mnist.w.wm --epsilon_max 100
-python3 WatermarkVerification1.py --model test --epsilon_max 0.5 --epsilon_interval 0.001
+python3 WatermarkVerification1.py --model mnist.w.wm --epsilon_max 0.5 --epsilon_interval 0.001
