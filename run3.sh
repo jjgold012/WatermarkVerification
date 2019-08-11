5d2f347124a49cc621b0eb765fb8eb2a06788d4b
JJG <j.j.goldberger@gmail.com>
2019-08-11 11:44:52 +0300

    blah

--- run3.sh
+++ run3.sh
@@ -3,5 +3,6 @@
 
 export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
 
+
 python3 WatermarkVerification3.py --model mnist.w.wm --epsilon_max 10 --num_of_inputs 4
 # python3 WatermarkVerification2.py --model test --epsilon_max 100
