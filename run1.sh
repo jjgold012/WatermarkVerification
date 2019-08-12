8a2259983e634ad12f1ada6532bfea2cd2214eef
JJG <j.j.goldberger@gmail.com>
2019-08-12 14:14:16 +0300

    slurm array test

--- run1.sh
+++ run1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH -c2
 #SBATCH --time=2-0
-#SBATCH --mem-per-cpu=2g
 
-export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
+# export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
 
-python3 WatermarkVerification1.py --model mnist.w.wm --epsilon_max 0.5 --epsilon_interval 0.001
+# python3 WatermarkVerification1.py --model mnist.w.wm --epsilon_max 0.5 --epsilon_interval 0.001
+
+echo $SLURM_ARRAY_TASK_ID
