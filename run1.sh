2fe0f345fc430864b37e891ad7060f6ae2fe21e8
JJG <j.j.goldberger@gmail.com>
2019-08-13 17:45:17 +0300

    changes to run problem 2

--- run1.sh
+++ run1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#SBATCH -c2
 #SBATCH --time=2-0
+#SBATCH --mem-per-cpu=4096
 #SBATCH --mail-type=BEGIN,END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=jjgold@cs.huji.ac.il    # Where to send mail	
 
