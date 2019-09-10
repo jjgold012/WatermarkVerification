0c6ee2637972cd3db39012a64d27394ccfde51a5
JJG <j.j.goldberger@gmail.com>
2019-09-10 10:37:12 +0300

    more problem 3 data

--- run3.sh
+++ run3.sh
@@ -3,5 +3,6 @@
 
 export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
 
-python3 WatermarkVerification3.py --model mnist.w.wm --epsilon_max 10 --num_of_inputs 2
+python3 WatermarkVerification3gurobi.py --model mnist.w.wm --num_of_inputs 50
+python3 WatermarkVerification3gurobi.py --model mnist.w.wm --num_of_inputs 75
 # python3 WatermarkVerification2.py --model test --epsilon_max 100
