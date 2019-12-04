d1a0c85a24e89132be17a39b948fbd0b36c6c8ff
JJG <j.j.goldberger@gmail.com>
2019-12-04 13:19:38 +0200

    run4 file

--- run4.sh
+++ run4.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#SBATCH --time=7-0
+
+#SBATCH -c2
+#SBATCH --time=2-0
+#SBATCH --mem-per-cpu=4096
+#SBATCH --mail-type=BEGIN,END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=jjgold@cs.huji.ac.il    # Where to send mail	
 
 export PYTHONPATH=$PYTHONPATH:"$(dirname "$(pwd)")"/Marabou
 
 python3 WatermarkVerification4.py --model mnist.w.wm
-# python3 WatermarkVerification2.py --model test --epsilon_max 100
+
