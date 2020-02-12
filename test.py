eaae240811d90c459c4ca45289ad7cd312eaf7f6
JJG <j.j.goldberger@gmail.com>
2020-02-12 11:29:13 +0200

    lp example

--- test.py
+++ test.py
@@ -4,30 +4,61 @@ import os
 import utils
 from tensorflow import keras
 from pprint import pprint
+from csv import DictReader, DictWriter
 
 model_name = 'mnist.w.wm'
 MODELS_PATH = './Models'
 
-epsilons = np.load('./data/results/problem4/{}.4.wm.vals.npy'.format(model_name))
-randomSamples = np.load('./data/random/4.wm.1000.random_samples.npy')
-wm_images = np.load('./data/wm.set.npy')
-wm_images = wm_images.reshape(wm_images.shape[0], wm_images.shape[1], wm_images.shape[2],1)
+# epsilons = np.load('./data/results/problem4/{}.4.wm.vals.npy'.format(model_name))
+# randomSamples = np.load('./data/random/4.wm.1000.random_samples.npy')
+# wm_images = np.load('./data/wm.set.npy')
+# wm_images = wm_images.reshape(wm_images.shape[0], wm_images.shape[1], wm_images.shape[2],1)
 
-for j in range(10):
-    net_model = utils.load_model(os.path.join(MODELS_PATH, model_name+'_model.json'), os.path.join(MODELS_PATH, model_name+'_model.h5'))
+# for j in range(10):
+#     net_model = utils.load_model(os.path.join(MODELS_PATH, model_name+'_model.json'), os.path.join(MODELS_PATH, model_name+'_model.h5'))
 
-    weights = net_model.get_weights()
-    weights[-1] = weights[-1] + epsilons[j]
+#     weights = net_model.get_weights()
+#     weights[-1] = weights[-1] + epsilons[j]
 
-    net_model.set_weights(weights)
-    net_model.compile(optimizer=tf.train.AdamOptimizer(),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-    predictions = np.load('./data/{}.prediction.npy'.format(model_name))
-    predIndices = np.flip(np.argsort(predictions, axis=1), axis=1)        
-    first = predIndices[:,0] 
-    second = predIndices[:,1]
-    newPred = net_model.predict(wm_images)
-    for i in randomSamples[j]:
-        print('original prediction: {}, new prediction: {}'.format(first[i], second[i]))
-        print(newPred[i])
-    c = np.array([newPred[i][first[i]] - newPred[i][second[i]] for i in randomSamples[j]])
-    print(c)
+#     net_model.set_weights(weights)
+#     net_model.compile(optimizer=tf.train.AdamOptimizer(),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    
+#     predictions = np.load('./data/{}.prediction.npy'.format(model_name))
+#     predIndices = np.flip(np.argsort(predictions, axis=1), axis=1)     
+#     first = predIndices[:,0] 
+#     second = predIndices[:,1]
+#     newPred = net_model.predict(wm_images)
+#     for i in randomSamples[j]:
+#         print('original prediction: {}, new prediction: {}'.format(first[i], second[i]))
+#         print(newPred[i])
+#     c = np.array([newPred[i][first[i]] - newPred[i][second[i]] for i in randomSamples[j]])
+#     print(c)
+
+
+datafile1 = open('./data/results/problem3/{}.1.wm.csv'.format(model_name))
+datafile2 = open('./data/results/problem2/{}.csv'.format(model_name))
+file_reader = DictReader(datafile1)
+sat_vals1 = np.array([float(line['sat-epsilon']) for line in file_reader])
+file_reader = DictReader(datafile2)
+sat_vals2 = np.array([float(line['sat-epsilon']) for line in file_reader])
+predictions = np.load('./data/{}.prediction.npy'.format(model_name))
+predIndices = np.flip(np.sort(predictions, axis=1), axis=1)     
+first = predIndices[:,0] 
+second = predIndices[:,1]
+diff = first-second
+a = np.argsort(diff)
+a = set(a[:50])
+b = np.argsort(sat_vals1)
+b = set(b[:50])
+c = np.argsort(sat_vals2)
+c = set(c[:50])
+print(len(a.intersection(b)))
+print(len(a.intersection(c)))
+a = np.argsort(diff)
+a = set(a[50:])
+b = np.argsort(sat_vals1)
+b = set(b[50:])
+c = np.argsort(sat_vals2)
+c = set(c[50:])
+print(len(a.intersection(b)))
+print(len(a.intersection(c)))
